60e0da52f1b93a0e01c9a75d46443fe46b53c5bf
Tsai-TsungLin <ss19960429ss@gmail.com>
2024-01-25 15:30:49 +0800

    [feat] 改用espn api (prod)

--- nba/help.go
+++ nba/help.go
@@ -2,9 +2,13 @@ package nba
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -113,4 +117,106 @@ func formatInjury(injuries []string) string {
 		formatted += injury + "\n  "
 	}
 	return formatted
+}
+
+// 取得近五場輸贏盤口資訊
+func getDish(searchTeam string) map[string]string {
+	var (
+		season string
+	)
+
+	year := time.Now()
+	month := time.Now().Format("01")
+	monthR, _ := strconv.ParseInt(month, 10, 64)
+	if monthR < 7 {
+		season = year.AddDate(-1, 0, 0).Format("06") + "-" + year.Format("06")
+	} else {
+		season = year.Format("06") + "-" + year.AddDate(1, 0, 0).Format("06")
+	}
+	yearR := year.Format("2006010215")
+
+	// http://nba.titan007.com/cn/LetGoal.aspx?SclassID=1&matchSeason=2022-2023
+	url := "https://nba.titan007.com/jsData/letGoal/" + season + "/l1.js?version=" + yearR
+
+	log.Println("URL: ", url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	sitemap, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var Team string
+
+	r1, _ := regexp.Compile(";")
+	r2 := r1.FindAllStringSubmatchIndex(string(sitemap), -1)
+
+	Team = (string([]byte(sitemap[r2[0][0]+17 : r2[1][0]-1])))
+	frontside, _ := regexp.Compile(`\[(.*?)]`)
+
+	frontsideTeam := frontside.FindAllStringSubmatchIndex(string(Team), -1)
+
+	matchMap := make(map[int64]string)
+	result := make(map[string]string)
+
+	for i := range frontsideTeam {
+		//把隊伍資料放進map，稍後用在拿勝率
+		TeamData := (Team[frontsideTeam[i][0]:frontsideTeam[i][1]])
+		match, _ := regexp.Compile(",")
+		matchR := match.FindAllStringSubmatchIndex(TeamData, -1)
+		TeamNumber, _ := strconv.ParseInt(TeamData[1:matchR[0][0]], 10, 64) //TeamData[1:TeamData[matchR[0][0]-1]]
+
+		if searchTeam == TeamData[matchR[2][1]+1:matchR[3][0]-1] {
+			matchMap[TeamNumber] = TeamData[matchR[2][1]+1 : matchR[3][0]-1]
+		}
+	}
+
+	data := (string([]byte(sitemap[r2[1][0]+20 : r2[2][0]-1])))
+	frontsidedata := frontside.FindAllStringSubmatchIndex(string(data), -1)
+
+	//拿取勝率等資料
+	for i := range frontsidedata {
+		winPercentData := (data[frontsidedata[i][0]:frontsidedata[i][1]])
+		match, _ := regexp.Compile(",")
+		matchR := match.FindAllStringSubmatchIndex(winPercentData, -1)
+		TeamNumber, _ := strconv.ParseInt(winPercentData[matchR[0][0]+1:matchR[1][0]], 10, 64)
+
+		//比對Map內是否有這個TeamNumber，有的話加入result map內回傳
+		if _, ok := matchMap[TeamNumber]; ok {
+
+			one := changeWinLose(winPercentData[matchR[11][0]+1 : matchR[12][1]-1])
+			two := changeWinLose(winPercentData[matchR[12][0]+1 : matchR[13][1]-1])
+			three := changeWinLose(winPercentData[matchR[13][0]+1 : matchR[14][1]-1])
+			four := changeWinLose(winPercentData[matchR[14][0]+1 : matchR[15][1]-1])
+			five := changeWinLose(winPercentData[matchR[15][0]+1 : matchR[15][1]+1])
+
+			result[matchMap[TeamNumber]] = one + "," + two + "," + three + "," + four + "," + five
+		}
+	}
+
+	return result
+
+}
+
+func changeWinLose(r string) (result string) {
+	if r == "0" {
+		result = "贏"
+	} else {
+		result = "輸"
+	}
+	return
+}
+
+// formatDish 格式化過盤狀況信息
+func formatDish(dish map[string]string, team string) string {
+	if result, ok := dish[team]; ok {
+		return result
+	}
+	return "無可用數據"
 }
--- nba/nba.go
+++ nba/nba.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sort"
+	"sync"
 	"time"
 )
 
@@ -49,6 +51,52 @@ type NBATeam2 struct {
 	} `json:"sports"`
 }
 
+type Event struct {
+	Date        time.Time `json:"date"`
+	Competitors []struct {
+		DisplayName string `json:"displayName"`
+		HomeAway    string `json:"homeAway"`
+	} `json:"competitors"`
+	Odds struct {
+		OverUnder   float64 `json:"overUnder"`
+		PointSpread struct {
+			Away struct {
+				Open struct {
+					Line string `json:"line"`
+				} `json:"open"`
+				Close struct {
+					Line string `json:"line"`
+				} `json:"close"`
+			} `json:"away"`
+			Home struct {
+				Open struct {
+					Line string `json:"line"`
+				} `json:"open"`
+				Close struct {
+					Line string `json:"line"`
+				} `json:"close"`
+			} `json:"home"`
+		} `json:"pointSpread"`
+		AwayTeamOdds struct {
+			Favorite bool `json:"favorite"`
+		} `json:"awayTeamOdds"`
+		HomeTeamOdds struct {
+			Favorite bool `json:"favorite"`
+		} `json:"homeTeamOdds"`
+	} `json:"odds"`
+}
+
+type Game struct {
+	Info string
+	Date time.Time
+}
+
+type TeamInfo struct {
+	TeamName string
+	Injuries []string
+	Dish     map[string]string
+}
+
 func GetNBATeams() {
 	startTime := time.Now()
 
@@ -90,58 +138,109 @@ func GetNBATeams() {
 	}
 
 	chineseTeam := TeamInit()
-	var games []string
+
+	// var games []string
 	var dateOfGames time.Time
 	matchCount := 0
+
+	var wg sync.WaitGroup
+	gamesChan := make(chan Game, len(nbaTeam.Sports[0].Leagues[0].Events))
+
 	for _, sport := range nbaTeam.Sports {
 		for _, league := range sport.Leagues {
 			for _, event := range league.Events {
-				var homeTeam, awayTeam string
-				for _, competitor := range event.Competitors {
-					if competitor.HomeAway == "home" {
-						homeTeam = competitor.DisplayName
-					} else if competitor.HomeAway == "away" {
-						awayTeam = competitor.DisplayName
+				wg.Add(1)
+				go func(event Event) {
+					defer wg.Done()
+					var homeTeam, awayTeam string
+					for _, competitor := range event.Competitors {
+						if competitor.HomeAway == "home" {
+							homeTeam = competitor.DisplayName
+						} else if competitor.HomeAway == "away" {
+							awayTeam = competitor.DisplayName
+						}
 					}
-				}
 
-				if dateOfGames.IsZero() {
-					// 設置日期為第一個事件的日期
-					dateOfGames = event.Date
-				}
+					if dateOfGames.IsZero() {
+						// 設置日期為第一個事件的日期
+						dateOfGames = event.Date
+					}
 
-				// 转换开赛时间为您所需的时区，这里假设为 UTC+8
-				startTime := event.Date.In(time.FixedZone("UTC+8", 8*60*60))
+					// 转换开赛时间为您所需的时区，这里假设为 UTC+8
+					startTime := event.Date.In(time.FixedZone("UTC+8", 8*60*60))
 
-				// 检查是否有赔率信息，并确定是哪个队伍让分
-				var oddsInfo string
-				if event.Odds.PointSpread.Away.Open.Line != "" {
-					if event.Odds.AwayTeamOdds.Favorite {
-						// 如果客队是让分的队伍
-						oddsInfo = fmt.Sprintf("初盤 %s %s, 大小分: %.1f ", chineseTeam[awayTeam], event.Odds.PointSpread.Away.Open.Line, event.Odds.OverUnder)
-					} else if event.Odds.HomeTeamOdds.Favorite {
-						// 如果主队是让分的队伍
-						oddsInfo = fmt.Sprintf("初盤 %s %s, 大小分: %.1f ", chineseTeam[homeTeam], event.Odds.PointSpread.Home.Open.Line, event.Odds.OverUnder)
+					// 检查是否有赔率信息，并确定是哪个队伍让分
+					var oddsInfo string
+					if event.Odds.PointSpread.Away.Open.Line != "" {
+						if event.Odds.AwayTeamOdds.Favorite {
+							// 如果客队是让分的队伍
+							oddsInfo = fmt.Sprintf("初盤 %s %s, 大小分: %.1f ", chineseTeam[awayTeam], event.Odds.PointSpread.Away.Open.Line, event.Odds.OverUnder)
+						} else if event.Odds.HomeTeamOdds.Favorite {
+							// 如果主队是让分的队伍
+							oddsInfo = fmt.Sprintf("初盤 %s %s, 大小分: %.1f ", chineseTeam[homeTeam], event.Odds.PointSpread.Home.Open.Line, event.Odds.OverUnder)
+						} else {
+							oddsInfo = "赔率信息不明确"
+						}
 					} else {
-						oddsInfo = "赔率信息不明确"
+						oddsInfo = "尚未開盤"
 					}
-				} else {
-					oddsInfo = "尚未開盤"
-				}
 
-				// 获取客队和主队的伤兵信息
-				awayTeamInjuries := getInjury(awayTeam)
-				homeTeamInjuries := getInjury(homeTeam)
-				matchCount++
-				// 将伤兵信息添加到比赛信息中
-				gameInfo := fmt.Sprintf("%d. %s %s %s (主)\n %s\n%s 傷兵:\n%s\n---------------------------------\n%s 傷兵:\n%s\n", matchCount, chineseTeam[awayTeam], startTime.Format("15:04"), chineseTeam[homeTeam], oddsInfo, chineseTeam[awayTeam], formatInjury(awayTeamInjuries), chineseTeam[homeTeam], formatInjury(homeTeamInjuries))
-				games = append(games, gameInfo)
+					// 创建通道用于接收伤兵和盘口信息
+					infoChannel := make(chan TeamInfo, 2)
+
+					// 使用goroutine获取客队和主队的伤兵信息
+					go func(teamName string) {
+						injuries := getInjury(teamName)
+						dish := getDish(teamName)
+						infoChannel <- TeamInfo{TeamName: teamName, Injuries: injuries, Dish: dish}
+					}(awayTeam)
+
+					go func(teamName string) {
+						injuries := getInjury(teamName)
+						dish := getDish(teamName)
+						infoChannel <- TeamInfo{TeamName: teamName, Injuries: injuries, Dish: dish}
+					}(homeTeam)
+
+					// 从通道中收集数据
+					awayTeamInfo := <-infoChannel
+					homeTeamInfo := <-infoChannel
+
+					matchCount++
+					// 将伤兵信息和盘口信息添加到比赛信息中
+					gameInfo := fmt.Sprintf("%s %s %s (主)\n%s\n\n%s傷兵名單\n---------------------------------\n%s\n---------------------------------\n%s傷兵名單:\n%s\n%s 近五場盤口: %s\n%s 近五場盤口: %s\n",
+						chineseTeam[awayTeam], startTime.Format("15:04"), chineseTeam[homeTeam],
+						oddsInfo,
+						chineseTeam[awayTeamInfo.TeamName], formatInjury(awayTeamInfo.Injuries),
+						chineseTeam[homeTeamInfo.TeamName], formatInjury(homeTeamInfo.Injuries),
+						chineseTeam[awayTeamInfo.TeamName], formatDish(awayTeamInfo.Dish, awayTeamInfo.TeamName),
+						chineseTeam[homeTeamInfo.TeamName], formatDish(homeTeamInfo.Dish, homeTeamInfo.TeamName))
+
+					gamesChan <- Game{Info: gameInfo, Date: event.Date}
+
+					close(infoChannel)
+				}(event)
 			}
 		}
 	}
+	go func() {
+		wg.Wait()
+		close(gamesChan)
+	}()
 
-	fmt.Printf("今天 %s 有 %d 場比賽\n", dateOfGames.Format("2006-01-02"), len(games))
-	for i, game := range games {
-		fmt.Printf("%d. %s\n", i+1, game)
+	var gameAll []Game
+	for game := range gamesChan {
+		gameAll = append(gameAll, game)
 	}
+
+	// 按日期时间排序
+	sort.Slice(gameAll, func(i, j int) bool {
+		return gameAll[i].Date.Before(gameAll[j].Date)
+	})
+	fmt.Printf("今天 %s 有 %d 场比赛\n\n", dateOfGames.Format("2006-01-02"), len(gameAll))
+	// 输出排序后的比赛信息
+	for i, game := range gameAll {
+		fmt.Printf("%d. %s\n", i+1, game.Info)
+	}
+
+	fmt.Printf("耗时：%v\n", time.Since(startTime))
 }
